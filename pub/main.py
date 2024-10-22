a3cbc61fd31c8f62661ebd39d5c4c57c4e80b464
Kimcheolhui <cjfgml0306@naver.com>
2024-10-22 21:35:36 +0900

    update: change camera resolution, modify position adjustment logic

--- pub/main.py
+++ pub/main.py
@@ -80,9 +80,17 @@ def pred_depth_frame(frame):
 
 
 # 카메라 스트림 캡처 및 NATS 전송
-async def capture_and_send_video(tag_id, subject):
+async def capture_and_send_video(tag_id, subject, uwb, direction):
     nc = await connect_to_nats()
     cap = cv2.VideoCapture(0)
+
+    # 카메라 해상도 640 x 360
+    # 1920 x 1080, 1280 x 720, 960 x 540도 가능함
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
+
+    uwb_x = uwb[0]
+    uwb_y = uwb[1]
 
     while True:
         capture_start = time.time()
@@ -98,73 +106,116 @@ async def capture_and_send_video(tag_id, subject):
 
         # tag_id의 uwb 센서의 현재 좌표
         # TODO: uwb 정보 가져오기 위한 POD 재배포하기
-        uwb = str(requests.get('http://210.125.85.31:31008/uwb/' +
-                  str(tag_id)).text)[1:-1].split("_")
+        # uwb = str(requests.get('http://210.125.85.31:31008/uwb/' +
+        #           str(tag_id)).text)[1:-1].split("_")
 
         results = model(frame)  # YOLO object detection
         if len(results.xyxy[0]) > 0:  # 사람이 감지된 경우
-            detected = False
+            # detected = False # 필요 없어짐
             centers = []
-            depths = []
-            x_position = uwb[1]
+            # depths = []
 
-            # Camera frame에 대한 Depth 추정 (UniDepth)
-            depth_pred = pred_depth_frame(frame)
+            # 모든 Yolo 결과 중 'person' 클래스만 필터링하고 신뢰도 높은 순으로 정렬
+            person_detections = [
+                result for result in results.xyxy[0] if int(result[5]) == 0]
+            person_detections = sorted(person_detections, key=lambda x: x[4], reverse=True)[
+                :3]  # 신뢰도 높은 순으로 상위 3개 선택
 
-            # 모든 Yolo frames에 대해
-            for result in results.xyxy[0]:
-                # 0번 클래스가 'person'인 경우
-                if int(result[5]) == 0:
-                    detected = True
+            # 선택된 'Person' 객체들에 대해 처리
+            if len(person_detections) > 0:
+
+                # Camera frame에 대한 Depth 추정 (UniDepth)
+                # 'Person' 객체가 0개면 굳이 Depth 추정을 할 이유가 없어서 현재 indent level로 코드 옮김
+                depth_pred = pred_depth_frame(frame)
+
+                for result in person_detections:
+
                     # 바운딩 박스 중심 좌표 계산
                     xmin, ymin, xmax, ymax = result[0], result[1], result[2], result[3]
                     cx = (xmin + xmax) / 2
                     cy = (ymin + ymax) / 2
-                    centers.append((cx.item(), cy.item()))
 
                     # Yolo frame에 대한 depth 가져오기
                     depth = float(
                         depth_pred[int(cx.item()), int(cy.item())] - 1)
-                    depths.append(depth)
 
-            # 'Person' frame이 적어도 1개 이상 있었을 경우
-            if detected:
+                    x_pos, y_pos = [0, 0]
 
-                # 간단 보정
-                # TODO: Depth의 방향 추정 제대로 구현할 것
-                if centers[0][0] < 170:
-                    x_position = float(x_position) - 1
-                elif centers[0][0] > 470:
-                    x_position = float(x_position) + 1
+                    # 좌표 간단 보정
+                    # 해상도 20 = 좌표 0.125라고 가정
+                    if direction == 'x+':
+                        x_pos = float(uwb_x + depth)
+                        y_pos = float(uwb_y)
+                        if cx < 320:
+                            gap = 320 - cx
+                            adjust = (gap / 20) * 0.125
+                            y_pos -= adjust
+                        else:
+                            gap = cx - 320
+                            adjust = (gap / 20) * 0.125
+                            y_pos += adjust
+                    elif direction == 'x-':
+                        x_pos = float(uwb_x - depth)
+                        y_pos = float(uwb_y)
+                        if cx < 320:
+                            gap = 320 - cx
+                            adjust = (gap / 20) * 0.125
+                            y_pos += adjust
+                        else:
+                            gap = cx - 320
+                            adjust = (gap / 20) * 0.125
+                            y_pos -= adjust
+                    elif direction == 'y+':
+                        y_pos = float(uwb_y + depth)
+                        x_pos = float(uwb_x)
+                        if cx < 320:
+                            gap = 320 - cx
+                            adjust = (gap / 20) * 0.125
+                            x_pos += adjust
+                        else:
+                            gap = cx - 320
+                            adjust = (gap / 20) * 0.125
+                            x_pos -= adjust
+                    else:  # y-
+                        y_pos = float(uwb_y - depth)
+                        x_pos = float(uwb_x)
+                        if cx < 320:
+                            gap = 320 - cx
+                            adjust = (gap / 20) * 0.125
+                            x_pos -= adjust
+                        else:
+                            gap = cx - 320
+                            adjust = (gap / 20) * 0.125
+                            x_pos += adjust
 
-                # JPEG로 인코딩하여 손실 압축 적용 (품질 80%)
-                encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
-                _, buffer = cv2.imencode('.jpg', frame, encode_param)
+                    # JPEG로 인코딩하여 손실 압축 적용 (품질 80%)
+                    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
+                    _, buffer = cv2.imencode('.jpg', frame, encode_param)
 
-                # 파일명 생성
-                filename = f"frame_{int(time.time())}.jpg"
-                # JSON 데이터 생성
-                data = {
-                    "filename": filename,
-                    "image": base64.b64encode(buffer).decode('utf-8'),
-                    "tag_id": uwb[4],  # position_17.78_-21.76_tagid_15
-                    "position_x": x_position,
-                    "position_y": float(uwb[2]) + float(depths[0]),
-                    "time": time_cap
-                }
+                    # 파일명 생성
+                    filename = f"frame_{int(time.time())}.jpg"
+                    # JSON 데이터 생성
+                    data = {
+                        "filename": filename,
+                        "image": base64.b64encode(buffer).decode('utf-8'),
+                        # "tag_id": uwb[4],  # position_17.78_-21.76_tagid_15
+                        "position_x": x_pos,
+                        "position_y": y_pos,
+                        "time": time_cap
+                    }
 
-                data_print = {
-                    "filename": filename,
-                    "image": "r4lwEXtvKB95SM1w5Ug9K9I+I8",
-                    "tag_id": uwb[4],  # position_17.78_-21.76_tagid_15
-                    "position_x": x_position,
-                    "position_y": float(uwb[2]) + float(depths[0]),
-                    "time": time_cap
-                }
-                print(data_print)
-                message = json.dumps(data)
+                    data_print = {
+                        "filename": filename,
+                        # "image": "r4lwEXtvKB95SM1w5Ug9K9I+I8",
+                        # "tag_id": uwb[4],  # position_17.78_-21.76_tagid_15
+                        "position_x": x_pos,
+                        "position_y": y_pos,
+                        "time": time_cap
+                    }
+                    print(data_print)
+                    message = json.dumps(data)
 
-                await nc.publish(subject, message.encode('utf-8'))
+                    await nc.publish(subject, message.encode('utf-8'))
 
         await asyncio.sleep(0.2)  # 200ms 딜레이 안하면 초당 4장, 100ms 딜레이는 초당 2장
 
@@ -183,5 +234,14 @@ if __name__ == "__main__":
                         help='to the input tag_id')  # tag_id: UWB 센서 ID
     parser.add_argument('--subject', type=str, required=False, default='Falcon.ternal.Group.A',
                         help='to the input tag_id')  # subject: nats subject
+
+    parser.add_argument('--xpos', type=float, required=True,
+                        help='to the uwb x position')
+    parser.add_argument('--ypos', type=float, required=True,
+                        help='to the uwb y position')
+
+    parser.add_argument('--direction', type=str, required=True,
+                        help='to the station direction, x+, x-, y+, y-')
     args = parser.parse_args()
-    asyncio.run(capture_and_send_video(args.tag_id, args.subject))
+    asyncio.run(capture_and_send_video(
+        args.tag_id, args.subject, [args.xpos, args.ypos], args.direction))
